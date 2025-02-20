b8d4aafc7a9ea21a4813b233f98a9861104daecb
Kutubkhan-Makda <makdakutubkhan@gmail.com>
2025-02-20 22:02:27 +0530

    fix: remove redundant login route definition in authRoutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,11 +4,3 @@ const AuthController = require('../controllers/authController');
 const router = express.Router();
 const authController = new AuthController();
 
-const setAuthRoutes = (app) => {
-    router.post('/register', authController.register);
-    router.post('/login', authController.login);
-    router.post('/login', authController.login);
-    app.use('/api/auth', router);
-};
-
-module.exports = setAuthRoutes;
